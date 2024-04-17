40def2c8b6b386c1b02f5feea5ddc8ac039e890d
You <you@example.com>
2024-04-17 22:54:52 +0800

    添加多线程支持

--- test.py
+++ test.py
@@ -1,15 +1,21 @@
-import re,tqdm,sys
+import re,tqdm,sys,pickle,os
 import subprocess,multiprocessing 
-
+from concurrent.futures import ThreadPoolExecutor
 def analyze_sentence(sentence):
     # 创建一个新的子进程来运行 link-parser
     process = subprocess.Popen(['link-parser'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     # 通过 stdin 提供句子
     stdout, stderr = process.communicate(sentence)
+    ana=""
+    for i in stdout.split("\n"):
+        if(i.startswith("LEFT-WALL")):
+            ana=i
     # 分析输出（这部分需要你根据你的需求来编写）
     if 'No complete linkages found.' in stdout:
-        return False
-    return True
+        return sentence,False,ana
+
+        
+    return sentence,True,ana
 def replace_words(text):
     # 将Eq替换为equation
     text = text.replace('Eq', 'equation')
@@ -44,11 +50,8 @@ def process_latex_file(file_path):
     content=remove_citations(content)
     content=replace_numbers(content)
     content=remove_brackets(content)
-    
-    
     # 匹配所有以大写字母开头，以句号"."结尾的句子
     sentences = re.findall(r'([A-Z][^\\}\{.]*\.\s)', content)
-
     return sentences
 def load_config(file_name):
     
@@ -58,7 +61,7 @@ def load_config(file_name):
     line_set=set()
     for line in lines:
         line_set.add(line.strip())  # 使用 strip() 方法移除每行末尾的换行符
-        print(line.strip())
+        #print(line.strip())
     print("所有",file_name,"已经被载入")
     return line_set
 def write_config(success_list,file_name):
@@ -71,35 +74,44 @@ def handle_file(file_name):
     # 打开文件
     line_list=load_config("ignore.txt")
     success_list=load_config("success.txt")
-    fail_list=load_config("fail.txt")
+    if not os.path.exists('fail.pkl'):
+        fail_dict={}
+    else:
+
+        fail_dict = pickle.load(open('fail.pkl', 'rb+') )
     # 测试
     sentences = process_latex_file(file_name)
     wrong_number=0
     ans=""
+    needs_calc=[]
     for sentence in tqdm.tqdm(sentences):
         sentence=sentence[:-1]
         ignore=False
         fails=False
         if(sentence in line_list or sentence in success_list):
             ignore=True
-            tqdm.tqdm.write(f'Ignore: {sentence}')
             continue
-        if(sentence in fail_list):
-            fails=True
-        if(fails==True or analyze_sentence(sentence)==False):
+        if(sentence in fail_dict):
+            ans+=fail_dict[sentence][10:]+"\n"
             wrong_number+=1
-            tqdm.tqdm.write(f'Error in sentence: {sentence}')
-            ans+=f'Error in sentence: {sentence}\n'
-            fail_list.add(sentence)
-        else: #加入到success中去
-            success_list.add(sentence)
-            
-    print("识别的句子总数:",len(sentences),"错误率:",wrong_number/len(sentences))
+            fails=True
+            continue
+        needs_calc.append(sentence)
 
-    with open("Ana_"+file_name+".txt", 'w') as f:
-        f.write(ans)
+    with ThreadPoolExecutor() as executor:
+        result_list = list(executor.map(analyze_sentence, needs_calc))
+    for sentence,flag,info in result_list:
+        if(flag==True):
+            success_list.add(sentence)
+        else:
+            wrong_number+=1
+            fail_dict[sentence]=info
+            ans+=info[10:]+"\n"
+    print("识别的句子总数:",len(sentences),"错误率:",wrong_number/len(sentences))
+    with open("Ana_"+file_name+".txt", 'w') as f:  f.write(ans)
+    with open('fail.pkl', 'wb') as file: pickle.dump(fail_dict, file)
     write_config(list(success_list),"success.txt")
-    write_config(list(fail_list),"fail.txt")
+    
 processes = []
 for i in sys.argv[1:]:
     handle_file(i)
