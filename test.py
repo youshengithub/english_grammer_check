47e13882f8e8ce4464c64b9f5bd723cb1669c38e
You <you@example.com>
2024-04-17 12:47:48 +0800

    命令行参数启动

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import re,tqdm
+import re,tqdm,sys
 import subprocess
 
 def analyze_sentence(sentence):
@@ -51,37 +51,41 @@ def process_latex_file(file_path):
 
     return sentences
 
+def handle_file(file_name):
+    
+    # 打开文件
+    with open('ignore.txt', 'r') as f:
+        # 读取所有行，每一行作为列表的一个元素
+        lines = f.readlines()
+    line_list=[]
+    for line in lines:
+        line_list.append(line.strip())  # 使用 strip() 方法移除每行末尾的换行符
+        print(line.strip())
+    print("所有ignore已经被载入")
+    # 测试
+    sentences = process_latex_file(file_name)
+    wrong_number=0
+    ans=""
+    for sentence in tqdm.tqdm(sentences):
+        sentence=sentence[:-1]
+        ignore=False
+        for ig in line_list:
+            if(ig==sentence):
+                ignore=True
+                break
+        if(ignore==True):
+            tqdm.tqdm.write(f'Ignore: {sentence}')
+            continue
+        if(analyze_sentence(sentence)==False):
+            wrong_number+=1
+            tqdm.tqdm.write(f'Error in sentence: {sentence}')
+            ans+=f'Error in sentence: {sentence}\n'
 
-# 打开文件
-with open('ignore.txt', 'r') as f:
-    # 读取所有行，每一行作为列表的一个元素
-    lines = f.readlines()
-line_list=[]
-for line in lines:
-    line_list.append(line.strip())  # 使用 strip() 方法移除每行末尾的换行符
-    print(line.strip())
-print("所有ignore已经被载入")
-# 测试
-sentences = process_latex_file('my_article.tex')
-wrong_number=0
-ans=""
-for sentence in tqdm.tqdm(sentences):
-    sentence=sentence[:-1]
-    ignore=False
-    for ig in line_list:
-        if(ig==sentence):
-            ignore=True
-            break
-    if(ignore==True):
-        tqdm.tqdm.write(f'Ignore: {sentence}')
-        continue
-    if(analyze_sentence(sentence)==False):
-        wrong_number+=1
-        tqdm.tqdm.write(f'Error in sentence: {sentence}')
-        ans+=f'Error in sentence: {sentence}'
+    print("识别的句子总数:",len(sentences),"错误率:",wrong_number/len(sentences))
 
-print("识别的句子总数:",len(sentences),"错误率:",wrong_number/len(sentences))
+    with open("Ana_"+filename+".txt", 'w') as f:
+        # 写入字符串
+        f.write(ans)
 
-with open('ana_res.txt', 'w') as f:
-    # 写入字符串
-    f.write(ans)
+for i in sys.argv[1:]:
+    handle_file(i)
